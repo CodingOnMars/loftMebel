05e7ac5723877098918de8c6080dab98c07741ef
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-08-17 16:58:07 +0300

    Added responsive header, global styles, svg icons. Modified gulp config to support svg sprites

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
-$(function () {
-    
-})
+// Draft burger menu
+const menuBurger = document.querySelector('.menu-burger');
+const menu = document.querySelector('.menu');
+const menuBtn = document.querySelector('.menu__btn');
+
+const openMenu = () => {
+	menu.classList.toggle('active');
+};
+
+menuBurger.addEventListener('click', openMenu);
+menuBtn.addEventListener('click', openMenu);
