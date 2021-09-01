35ed69801f17c20b8758a7f7696ad367d28af453
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-09-01 17:46:58 +0300

    Added new content for sale-hits. Added Load more button

--- app/js/main.js
+++ app/js/main.js
@@ -9,3 +9,23 @@ const openMenu = () => {
 
 menuBurger.addEventListener('click', openMenu);
 menuBtn.addEventListener('click', openMenu);
+
+// Load more button
+const loadMore = document.querySelector('.load-more');
+let currentItems = 4;
+loadMore.addEventListener('click', e => {
+	const elementList = [
+		...document.querySelectorAll('.sale-hits .sale-hits__item'),
+	];
+	for (let i = currentItems; i < currentItems + 4; i++) {
+		if (elementList[i]) {
+			elementList[i].classList.add('sale-hits__item--active');
+		}
+	}
+	currentItems += 4;
+
+	// Hide load more button after all items were loaded
+	if (currentItems >= elementList.length) {
+		e.target.style.display = 'none';
+	}
+});
