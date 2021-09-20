cad6fc5dba6d2b4d6572d55cbacba3c9c82a012d
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-09-20 17:31:31 +0300

    Added styles for desktop version. In index.html added anchor links to product-page. Added bestsellers block to product-page. Created separate module for loadMore functionality

--- app/js/index.js
+++ app/js/index.js
@@ -1,21 +1,2 @@
 import './modules/burgerMenu.js';
-
-// Load more button
-const showMoreBtn = document.querySelector('.show-more');
-let currentItems = 4;
-showMoreBtn.addEventListener('click', e => {
-	const elementList = [
-		...document.querySelectorAll('.sale-hits .sale-hits__item'),
-	];
-	for (let i = currentItems; i < currentItems + 4; i++) {
-		if (elementList[i]) {
-			elementList[i].classList.add('sale-hits__item--active');
-		}
-	}
-	currentItems += 4;
-
-	// Hide load more button after all items were loaded
-	if (currentItems >= elementList.length) {
-		e.target.style.display = 'none';
-	}
-});
+import './modules/loadMore.js';
--- app/js/pages/product-page.js
+++ app/js/pages/product-page.js
@@ -2,6 +2,7 @@ import '../modules/burgerMenu.js';
 import get from '../modules/getElement.js';
 import selectImg from '../modules/selectImg.js';
 import '../modules/changeSelectBackground.js';
+import '../modules/loadMore.js';
 
 const preview = get('.gallery__preview');
 
