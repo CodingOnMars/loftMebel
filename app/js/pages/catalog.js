f732cc6af7441376d8c7e5bbf8811ea850897883
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-09-22 20:29:30 +0300

    Added sale-hits block to HTML. Added core styles. Added basic JS to check filters opening. Minor tweaks and optimisations

--- app/js/pages/catalog.js
+++ app/js/pages/catalog.js
@@ -1,1 +1,18 @@
 import '../modules/burgerMenu.js';
+import get from '../modules/getElement.js';
+
+// Draft show/hide filter list
+const filterBtn = get('.sorting__filter-btn');
+const filters = get('.catalog__filters');
+
+filterBtn.addEventListener('click', () => {
+	filters.classList.toggle('catalog__filters--active');
+});
+
+// Draft Show/hide sort options
+const sortBtn = get('.sorting__sort-btn');
+const sortList = get('.sorting__list');
+
+sortBtn.addEventListener('click', () => {
+	sortList.classList.toggle('sorting__list--active');
+});
