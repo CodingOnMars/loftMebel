c0a633d05b1dac546f6201a71464b78fdda02d62
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-10-25 17:21:27 +0300

    Product-page.js refactoring. Removed obsolete loadMore.js module

--- app/js/pages/product-page.js
+++ app/js/pages/product-page.js
@@ -1,13 +1,20 @@
 import displayMobileMenu from '../modules/burgerMenu.js';
+import {
+	displayFourItems,
+	clickShowMore,
+} from '../modules/displayFourItems.js';
 import get from '../modules/getElement.js';
 import selectImg from '../modules/selectImg.js';
 import '../modules/changeSelectBackground.js';
-import { products } from '../local-data/products.js';
-import displayProducts from '../modules/displayProducts.js';
-import '../modules/loadMore.js';
 
+// Show burger menu
 displayMobileMenu();
-displayProducts(products);
+
+// Show 4 items in sale-hits
+displayFourItems();
+
+// Display next 4 items when click on 'show-more' button
+clickShowMore();
 
 const preview = get('.gallery__preview');
 
